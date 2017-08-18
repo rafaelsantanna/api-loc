1f2ecc082c8c1a55798a7ebd2c98a4cb9ec44a79
Rafael da Silva <rafaeel.16@gmail.com>
2017-08-18 02:24:24 +0000

    Crud PositionsController Finalizado

--- app/Http/Controllers/PositionsController.php
+++ app/Http/Controllers/PositionsController.php
@@ -10,5 +10,60 @@ use App\Position;
 
 class PositionsController extends Controller
 {
-    //
+    public function index()
+    {
+        $positions = Position::all();
+        return response()->json($positions);
+    }
+    
+    public function show($id)
+    {
+        $position = Position::find($id);
+        
+        if(!$position){
+            return response()->json([
+                'message' => 'Record not found',
+                ], 404);
+        }
+        
+        return response()->json($position);
+    }
+    
+    public function store(Request $request)
+    {
+        $position = new Position();
+        $position->fill($request->all());
+        $position->save();
+        
+        return response()->json($request,201);
+    }
+    
+    public function update(Request $request, $id)
+    {
+        $position = Position::find($id);
+        
+        if(!$position) {
+            return response()->json([
+                'message' => 'Record not found',
+            ], 404);
+            
+        $position->fill($request->all());
+        $position->save();
+        
+        return response()->json($position);
+        }
+    }
+    
+    public function destroy($id)
+    {
+        $position = Position::find($id);
+        
+        if(!$position){
+            return response()->json([
+                'message' => 'Record not found',
+            ], 404);
+        }
+        
+        $position->delete();
+    }
 }
